5d94eb0c4b0ee77d29ef46e0c545312213eca6e9
choich <ghks9791@naver.com>
2017-10-13 14:00:06 +0900

    서블릿 utf-8

--- WebClass/src/org/dimigo/servlet/BlogLoginServlet.java
+++ WebClass/src/org/dimigo/servlet/BlogLoginServlet.java
@@ -24,7 +24,7 @@ public class BlogLoginServlet extends HttpServlet {
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.setCharacterEncoding("utf-8");
-        //response.setContentType("text/html;charset=utf-8");
+        response.setContentType("text/html;charset=utf-8");
 
         String id = request.getParameter("id");
         String password = request.getParameter("password");
